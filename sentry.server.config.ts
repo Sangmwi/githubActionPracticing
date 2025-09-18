b73dde516ca02cbe4b79097c73cf939accf22b09
Sangmwi Han <gkstkdgnl12@naver.com>
2025-09-18 15:41:26 +0900

    깃허브 액션 설정 수정 및 테스트 환경 구성 개선, Sentry 통합 추가, Playwright 설정 파일 삭제

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://585d611e4962ad6016cbdfd6b8397e83@o4510038643245056.ingest.us.sentry.io/4510038646521856",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
